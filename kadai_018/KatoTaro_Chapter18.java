842ee3807b70a223444703c91d8a5abbeac31d20
arumakan99 <162653895+arumakan99@users.noreply.github.com>
2024-03-13 11:57:40 +0900

    KatoTaro_Chapter18.javaを修正

--- kadai_018/KatoTaro_Chapter18.java
+++ kadai_018/KatoTaro_Chapter18.java
@@ -12,7 +12,7 @@ public class KatoTaro_Chapter18 extends Kato_Chapter18 {
 	
 	//セッター
 	public void setGivenName(String givenName) {
-		this.givenName="太郎";
+		this.givenName=givenName;
 	}
 	
 	//抽象メソッドのオーバーライド
